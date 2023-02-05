e326e5b45bd615e9c11f03019877da37fccdc0f0
OudayAhmed <66073097+OudayAhmed@users.noreply.github.com>
2023-02-05 19:28:50 +0100

    Updated the main file

--- main.py
+++ main.py
@@ -15,25 +15,17 @@ app = Flask(__name__)
 def api_root():
     return "Test 1"
 
-
-@app.route('/github', methods=['POST'])
+@app.route('/', methods=['POST'])
 def api_git_new_issue():
-    if request.headers['Content-Type'] == 'application/json':
+    info = request.json
+    path = os.getcwd()
+    print(path)
+    f = tempfile.mkdtemp(dir=path)
+    print(f)
+    Repo.clone_from(info['repository']['clone_url'], f, branch=info['repository']['default_branch'])
+    rmtree(f)
 
-        info = request.get_json()
-        print(info)
-
-        path = os.getcwd()
-        print(path)
-        f = tempfile.mkdtemp(dir=path)
-        print(f)
-        Repo.clone_from(info['repository']['clone_url'], f, branch=info['repository']['default_branch'])
-        path_main = f + '\main.py'
-        subprocess.call(['python', path_main], shell=True)
-        rmtree(f)
-
-        return "Test 2"
-
+    return "Test 2"
 
 if __name__ == '__main__':
-    app.run(port=8015, debug=True)
+    app.run(host="localhost", port=8015, debug=True)
