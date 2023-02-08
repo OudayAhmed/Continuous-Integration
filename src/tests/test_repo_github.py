b6a5b7f352c672b7cf22997260c9634d88b7290d
YimingJu <yimingj@kth.se>
2023-02-08 12:33:20 +0100

    clone repo test

--- src/tests/test_repo_github.py
+++ src/tests/test_repo_github.py
@@ -1,10 +1,24 @@
+import os.path
 from unittest import TestCase
+from ..app.repo_github import RepoGitHub
+
 
 
 class TestRepoGitHub(TestCase):
 
-    def test_clone_repo(self):
-        self.fail()
+    def test_clone_repo_pos(self):
+        with open("test_clone_repo_pos.txt", "r") as f:
+            dataJSON = f.read()
+            repoGitHub = RepoGitHub(dataJSON)
+            repoGitHub.cloneRepo()
+        self.assertTrue(os.path.isdir())
+
+    def test_clone_repo_neg(self):
+        with open("test_clone_repo_neg.txt", "r") as f:
+            dataJSON = f.read()
+            repoGitHub = RepoGitHub(dataJSON)
+            repoGitHub.cloneRepo()
+        self.assertFalse(os.path.isdir())
 
     def test_remove_repo(self):
         self.fail()
