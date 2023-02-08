dbc3bf4e9d1577914bda833bbd19c793c6b025a0
OudayAhmed <oydddua@gmail.com>
2023-02-08 16:37:21 +0100

    Added two negative tests for cloneRepo and removeRepo methods.

--- src/tests/test_repo_github.py
+++ src/tests/test_repo_github.py
@@ -6,5 +6,11 @@ class TestRepoGitHub(TestCase):
     def test_clone_repo(self):
         self.fail()
 
+    def test_clone_repo_negative(self):
+        self.fail()
+
     def test_remove_repo(self):
+        self.fail()
+
+    def test_remove_repo_negative(self):
         self.fail()
