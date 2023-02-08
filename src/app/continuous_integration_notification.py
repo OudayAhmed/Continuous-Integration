800b30c4e632eb616caf1f1d46fce5c94050fa4d
chrvik <90213900+ChristoferVikstroem@users.noreply.github.com>
2023-02-08 15:15:05 +0100

    Working on notifications
    Implemented skeleton and htmls.

---
+++ src/app/continuous_integration_notification.py
@@ -0,0 +1,37 @@
+from flask import request, Flask, render_template
+from flask_mail import Mail, Message
+
+from continuous_integration import ContinuousIntegration
+from repo_github import RepoGitHub
+import os
+
+app = Flask(__name__)
+mail = Mail(app)
+app.config['MAIL_SERVER'] = 'localhost'
+app.config['MAIL_POST'] = 25
+app.config['MAIL_USERNAME'] = None
+app.config['MAIL_PASSWORD'] = None
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_ASCII_ATTACHMENTS'] = False
+
+
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+@app.route('/send_message', methods=['GET', 'POST'])
+def send_message():
+    if request.method == "POST":
+        email = request.form['email']
+        subject = request.form['subject']
+        msg = request.form['message']
+        message = Message(subject, sender="christofervikstroem@gmail.com", recipients=[email])
+        message.body = msg
+        mail.send("message")
+        success = "Message sent"
+        return render_template("results.html", success=success)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8015)
