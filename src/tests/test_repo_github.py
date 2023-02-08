17032140cbe31aba8779b92e73a7fe8d903c29f8
YimingJu <yimingj@kth.se>
2023-02-08 13:13:09 +0100

    add discription to test_clone_repo

--- src/tests/test_repo_github.py
+++ src/tests/test_repo_github.py
@@ -2,11 +2,20 @@ import os.path
 from unittest import TestCase
 from ..app.repo_github import RepoGitHub
 
+"""TestRepoGitHub"""
 
 
 class TestRepoGitHub(TestCase):
+    """TestRepoGitHub class
+
+    contains the test cases for repository cloning and removing
+    """
 
     def test_clone_repo_pos(self):
+        """Positive test for cloning repository
+
+        Check whether the function cloneRepo() succeeds to clone the repo to local a local directory
+        """
         with open("test_clone_repo_pos.txt", "r") as f:
             dataJSON = f.read()
             repoGitHub = RepoGitHub(dataJSON)
@@ -14,11 +23,30 @@ class TestRepoGitHub(TestCase):
         self.assertTrue(os.path.isdir())
 
     def test_clone_repo_neg(self):
+        """Negative test for cloning repository
+
+        Check whether the function cloneRepo() fails to clone the repo to local a local directory
+        """
         with open("test_clone_repo_neg.txt", "r") as f:
             dataJSON = f.read()
             repoGitHub = RepoGitHub(dataJSON)
             repoGitHub.cloneRepo()
         self.assertFalse(os.path.isdir())
 
-    def test_remove_repo(self):
-        self.fail()
+    def test_remove_repo_pos(self):
+        with open("test_clone_repo_pos.txt", "r") as f:
+            dataJSON = f.read()
+            repoGitHub = RepoGitHub(dataJSON)
+            repoGitHub.cloneRepo()
+            repoGitHub.removeRepo()
+        self.assertFalse(os.path.isdir())
+
+    def test_remove_repo_neg(self):
+        with open("test_clone_repo_pos.txt", "r") as f:
+            dataJSON = f.read()
+            repoGitHub = RepoGitHub(dataJSON)
+            repoGitHub.cloneRepo()
+            wrong_path = " "
+            repoGitHub.repo_path = wrong_path
+            repoGitHub.removeRepo()
+        self.assertTrue(os.path.isdir())
