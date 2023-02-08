e022a016d04e8ee1d1bb8fb2a4a908f8faf3b11f
OudayAhmed <oydddua@gmail.com>
2023-02-08 16:40:59 +0100

    Fix issue/16
    Modified and updated the code for installRequirements, staticSyntaxCheck and testing methods.
    Added three for installRequirements, staticSyntaxCheck and testing methods.

--- src/tests/test_continuous_integration.py
+++ src/tests/test_continuous_integration.py
@@ -6,8 +6,17 @@ class TestContinuousIntegration(TestCase):
     def test_install_requirements(self):
         self.fail()
 
+    def test_install_requirements_negative(self):
+        self.fail()
+
     def test_static_syntax_check(self):
         self.fail()
 
+    def test_static_syntax_check_negative(self):
+        self.fail()
+
     def test_testing(self):
+        self.fail()
+
+    def test_testing_negative(self):
         self.fail()
