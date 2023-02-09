74cb5616c040562e7d2af25b4aa8907f15b1dc30
chrvik <90213900+ChristoferVikstroem@users.noreply.github.com>
2023-02-09 10:12:41 +0100

    Method change and implementation
    Changed the method and implemented it into continuous_integration_server.py

--- src/app/continuous_integration_notification.py
+++ src/app/continuous_integration_notification.py
@@ -1,44 +1,20 @@
 from flask import request, Flask, render_template
 from flask_mail import Mail, Message
 
-from continuous_integration import ContinuousIntegration
-from repo_github import RepoGitHub
-
-
-team_dict = {}
-team_dict['OudayAhmed'] = "oydddua@gmail.com"
-team_dict['ChristoferVikstroem'] = "christofer.vikstrom@outlook.com"
-team_dict['eliu1217'] = "eliu@kth.se"
-team_dict['OscarKnowles'] = "Oscar@knowles.se"
-team_dict['Taomyee'] = "yimingju2000@gmail.com"
-
 app = Flask(__name__)
 mail = Mail(app)
-
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_POST'] = 465
-app.config['MAIL_USERNAME'] = "cigroup15vt23@gmail.com"
-app.config['MAIL_PASSWORD'] = "contintg15"
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 @app.route('/send_message', methods=['GET', 'POST'])
-def send_message():
+def send_message(email, syntax, testing):
     if request.method == "POST":
-        email = "chrvik@kth.se" ## request.form['email'] ## Check git user and get value from dict to set email
-        subject = "Build Results"
-        msg = "Example: 3 of 4 tests completed." ## request.form['message'] ## Read and write the build results as the message
+        subject = "Build Results."
+        msg = str(syntax) + "\n" + str(testing) ## request.form['message'] ## Read and write the build results as the message
         message = Message(subject, sender="cigroup15vt23@gmail.com", recipients=[email])
         message.body = msg
         mail.send(message)
         success = "Message sent"
         return render_template("results.html", success=success)
-
-
-if __name__ == '__main__':
-    app.run(host='localhost', port=8015, debug=True)
