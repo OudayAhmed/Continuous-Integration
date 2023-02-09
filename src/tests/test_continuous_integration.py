6eb42769cccf1d059915cc85c2a270bc8e302445
oscarknowles <oscar@knowles.se>
2023-02-09 23:04:52 +0100

    Revert "Preliminary tests for install_requirements method"
    This reverts commit 10e5dbf4ec9a2baf4822f63a74fecde697e4eb91.

--- src/tests/test_continuous_integration.py
+++ src/tests/test_continuous_integration.py
@@ -1,26 +1,22 @@
 from unittest import TestCase
-from ..app.continuous_integration import ContinuousIntegration
-import os
+
 
 class TestContinuousIntegration(TestCase):
 
-    def test_install_requirements_positive(self):
-        ciTest = ContinuousIntegration(repo_path=os.getcwd(), resultFileName="testFile.txt", pathOSResults="/results", pathOSSrc="/src")
-        ciTest.installRequirements()
-        self.assertTrue(ciTest.isRequirementsInstalled)
+    def test_install_requirements(self):
+        self.fail()
 
     def test_install_requirements_negative(self):
-        ciTest = ContinuousIntegration(repo_path=os.getcwd(), resultFileName="testFile.txt", pathOSResults="/results", pathOSSrc="/src")
-        self.assertFalse(ciTest.installRequirements())
+        self.fail()
 
-    def test_static_syntax_check_positive(self):
+    def test_static_syntax_check(self):
         self.fail()
 
     def test_static_syntax_check_negative(self):
         self.fail()
 
-    def test_testin_positive(self):
+    def test_testing(self):
         self.fail()
 
-    def test_testing_negative_positive(self):
+    def test_testing_negative(self):
         self.fail()
