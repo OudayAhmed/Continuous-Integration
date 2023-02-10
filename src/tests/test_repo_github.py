bb82be7a6e35703dbeadbaa2bab7018d3669148a
Yiming Ju <112627752+Taomyee@users.noreply.github.com>
2023-02-10 12:12:34 +0100

    Fix issue #10 (#33)
    * clone repo test
    
    * add discription to test_clone_repo
    
    * pos neg test & discription
    
    * add testcases
    
    * update test_repo due to modification of main structure
    
    ---------
    
    Co-authored-by: YimingJu <yimingj@kth.se>

--- src/tests/test_repo_github.py
+++ src/tests/test_repo_github.py
@@ -1,16 +1,69 @@
+import json
+import os.path
 from unittest import TestCase
+from src.app.repo_github import RepoGitHub
+"""TestRepoGitHub"""
 
 
 class TestRepoGitHub(TestCase):
+    """TestRepoGitHub class
 
-    def test_clone_repo(self):
-        self.fail()
+    contains the test cases for repository cloning and removing
+    """
 
-    def test_clone_repo_negative(self):
-        self.fail()
+    def test_clone_repo_pos(self):
+        """Positive test for cloning repository
 
-    def test_remove_repo(self):
-        self.fail()
+        Check whether the function cloneRepo() succeeds to clone the repo to a local directory
+        """
+        with open("testcases/test_clone_repo_pos.txt", "r") as f:
+            dataJSON_str = f.read()
+            dataJSON = json.loads(dataJSON_str)
+            repoGitHub = RepoGitHub(dataJSON)
+            repoGitHub.cloneRepo("result.txt")
+        self.assertTrue(os.path.isdir(repoGitHub.repo_folder_name))
+        repoGitHub.removeRepo("result.txt")
 
-    def test_remove_repo_negative(self):
-        self.fail()
+    def test_clone_repo_neg(self):
+        """Negative test for cloning repository
+
+        Check whether the function cloneRepo() fails to clone the repo to a local directory
+        """
+        with open("testcases/test_clone_repo_neg.txt", "r") as f:
+            dataJSON_str = f.read()
+            dataJSON = json.loads(dataJSON_str)
+            repoGitHub = RepoGitHub(dataJSON)
+            repo_folder_name_origin = repoGitHub.repo_folder_name
+            repoGitHub.repo_folder_name = " "
+            repoGitHub.cloneRepo("result.txt")
+        self.assertFalse(os.path.isdir(repo_folder_name_origin))
+        repoGitHub.removeRepo("result.txt")
+
+    def test_remove_repo_pos(self):
+        """Positive test for removing repository
+
+        Check whether the function removeRepo() succeeds to remove the repo from a local directory
+        """
+        with open("testcases/test_clone_repo_pos.txt", "r") as f:
+            dataJSON_str = f.read()
+            dataJSON = json.loads(dataJSON_str)
+            repoGitHub = RepoGitHub(dataJSON)
+            repoGitHub.cloneRepo("result.txt")
+            repoGitHub.removeRepo("result.txt")
+        self.assertFalse(os.path.isdir(repoGitHub.repo_folder_name))
+
+    def test_remove_repo_neg(self):
+        """Negative test for removing repository
+
+        Check whether the function removeRepo() fails to remove the repo from a local directory
+        """
+        with open("testcases/test_clone_repo_pos.txt", "r") as f:
+            dataJSON = f.read()
+            repoGitHub = RepoGitHub(dataJSON)
+            repoGitHub.cloneRepo("result.txt")
+            wrong_path = " "
+            repoGitHub.repo_path_name = wrong_path
+            repoGitHub.removeRepo("result.txt")
+        self.assertTrue(os.path.isdir(repoGitHub.repo_folder_name))
+
+
